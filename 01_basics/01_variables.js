d37e8e11c1458458b1b2ac102376d7c8e0443686
lakshya981 <104674989+lakshya981@users.noreply.github.com>
2025-07-16 15:25:03 +0000

    container changes done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountID = 213533
+let accountemail = "lakshyasrivastava3461@gmail.com"
+var accountpassword = " Ballia"
+accountcity = " Ballia"
+ //node 01_basics_accountID = 2113322
+ accountemail = " lakshya.21gcebit101@gcet"
+ accountpassword = " ghazipur"
+ accountcity = " ghazipur"
+
+ console.table([accountID, accountemail, accountpassword, accountcity])
